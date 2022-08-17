d37b0a2fd7de1642c10af15c37300ef7c2ec6d42
Karol Wieliczko <58177400+kwieliczko@users.noreply.github.com>
2022-08-17 19:14:25 +0200

    Update main.py

--- Scrapper/main.py
+++ Scrapper/main.py
@@ -9,47 +9,35 @@ import re
 import sys
 from dataclasses import replace
 
-
-# Liczba komentarzy na pojedynczej stronie
+# Number of comments on a single page
 number_of_comments_in_one_page = 15
 
-
 url = 'https://www.nazwa-portalu.pl/'
-
 
 cur_dir = os.path.dirname(__file__)
 db_path = os.path.join(cur_dir, '..\Share\db\db.sqlite')
 
-        
 class main():
-    
     @staticmethod
     def run():
 
         http_request_obj = http_request.http_connect_class()
-        
         functions_obj = functions.funcions_class()
-        
         db_obj = sqlite.db_sql_lite_class()
         conn = db_obj.open_database(db_path)   
-
-        # Pobieram liczbe wszystkich podstron (na dole strony jest ta liczba)
   
-        # Zaczynamy od strony: 
+        # We start with the page: 
         j = 1
-
         soup = http_request_obj.get_html(url)
         last_page = http_request_obj.get_lastpage(soup)
        
         while j <= last_page:
-            
             url = site_url+product_url+str(j)+end_url
             try:
                 soup = http_request_obj.get_html(url)
-            
                           
             except Exception as e:
-                print("******** Wystapil problem:", e)
+                print("******** Something went wrong:", e)
                 pass
            
             rating_star_list = soup.findAll('span', {'class': re.compile("^review_badge*")})
@@ -60,28 +48,21 @@ class main():
 
             i = 0
             for  row in  rating_star_list:
-                
-                
+
                 try:
-                    
                     rating_star = row.getText().replace("/ 5","")   #.replace(".0","")
                     text_review = text_review_list[i].getText()
-                        
                     text_review = functions_obj.clean_unnecessary_characters(text_review)
-
                         
                     db_obj.save_data_in_database(conn, db_path, rating_star, text_review)
-                    
                     i += 1
 
                 except  Exception as e:
                     print("******** Wystapil problem:", e)
                     pass
                 
-                           
-            # Stopka
+            # Footer
             print(j,"/",last_page, "=>" ,url)
-            
             j += 1
                   
         print("\nDONE")
