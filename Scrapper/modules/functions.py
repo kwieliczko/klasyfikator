c65862932c1ac49f6a95519a6a3f541408d06a85
Karol Wieliczko <58177400+kwieliczko@users.noreply.github.com>
2022-08-17 19:15:30 +0200

    Update functions.py

--- Scrapper/modules/functions.py
+++ Scrapper/modules/functions.py
@@ -1,21 +1,14 @@
 import time
 
-class funcions_class:
-    
+class funcions_class:  
     def wait_time(self, seconds):
-
         while seconds > 0: 
             #print(".",end="")
             time.sleep(1)
             seconds -= 1
         
-        
-        
-        
-    def clean_unnecessary_characters(self, string_to_replace):
-        
+    def clean_unnecessary_characters(self, string_to_replace):      
         string_to_replace = string_to_replace.replace("\"", "")
         string_to_replace = string_to_replace.replace("\t","")
-        string_to_replace = string_to_replace.replace("\n","")
-        
+        string_to_replace = string_to_replace.replace("\n","")       
         return string_to_replace
