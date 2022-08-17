da2edd01b04d220974d41befd5bc7d01ef836281
Karol Wieliczko <58177400+kwieliczko@users.noreply.github.com>
2022-08-17 19:22:40 +0200

    Update http_request.py

--- Scrapper/modules/http_request.py
+++ Scrapper/modules/http_request.py
@@ -2,23 +2,15 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 class http_connect_class:
-    
-    # Pobiera caly kod HTML
+    # Downloads all HTML code
     def get_html(self, url):
-
         request = urllib.request.Request(url)
         response = urllib.request.urlopen(request)
-    
         soup = BeautifulSoup(response, 'html.parser')
-        
-    
         return soup
     
-    
-    # Ostatnia strona w tym dziale
+    # The last page in this section
     def get_lastpage(self, soup):
- 
         last_page = soup.findAll("span", {"class": "pagi_page"})
-        
         return int(last_page[-1].getText())
         
